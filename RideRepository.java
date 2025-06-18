cd1b54b81875431a172958d16e4e4cf84f0fdc72
Nandyala Naga Tejaswini <130234448+Aswini147@users.noreply.github.com>
2025-06-18 12:43:46 +0530

    Add files via upload

---
+++ RideRepository.java
@@ -0,0 +1,20 @@
+// src/main/java/com/cabsy/backend/repositories/RideRepository.java
+package com.cabsy.backend.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cabsy.backend.models.Ride;
+import com.cabsy.backend.models.RideStatus;
+
+@Repository
+public interface RideRepository extends JpaRepository<Ride, Long> {
+    List<Ride> findByUserId(Long userId);
+    List<Ride> findByDriverId(Long driverId);
+    List<Ride> findByStatus(RideStatus status);
+    List<Ride> findByUserIdAndStatus(Long userId, RideStatus status);
+    List<Ride> findByDriverIdAndStatus(Long driverId, RideStatus status);
+    
+}
